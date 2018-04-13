b2cc287b3c88716462b29eedc741ec9b39137ef7
Patrick Robinson <autarch@outlook.com>
2018-04-13 10:35:13 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'A cookbook to manage users from a data bag'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.8'
+version '0.2.9'
 
 supports 'centos'
 supports 'ubuntu'
