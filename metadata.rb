722eeb58e91e6a14584368ed2283bb3913fbb411
Telahr, Stephan <stephan.telahr@bertelsmann.de>
2018-04-19 06:29:58 +0000

    multi-os

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
-# Encoding: utf-8
-
 name 'system_users'
 maintainer 'Philipp Hellmich'
 maintainer_email 'philipp.hellmich@bertelsmann.de'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'A cookbook to manage users from a data bag'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_version '>= 12'
 version '0.3.0'
 
 supports 'centos'
