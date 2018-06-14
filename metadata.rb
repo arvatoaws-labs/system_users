bf1b5f74ddd5cce57c219bbb13460aeede07c23c
Frederik S <frederik.schubert@bertelsmann.de>
2018-06-14 11:15:51 +0200

    windows improvements

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,11 @@ license 'Apache-2.0'
 description 'A cookbook to manage users from a data bag'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 12'
-version '0.3.0'
+version '0.3.1'
 
 supports 'centos'
 supports 'ubuntu'
+supports 'windows'
 
 depends 'user'
 depends 'sudo'
